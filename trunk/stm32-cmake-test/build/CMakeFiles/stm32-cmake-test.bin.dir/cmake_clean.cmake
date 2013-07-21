bdec75f290182ec6e560664a3f4c49b1d0a5d143
lukasz.iwaszkiewicz@gmail.com <lukasz.iwaszkiewicz@gmail.com@e6fd12c7-fa05-65f5-2425-0ae74a4147e3>
2013-07-21 21:29:58 +0000

    Share project "stm32-cmake-test" into "https://iwasz-sandbox.googlecode.com/svn"

---
+++ trunk/stm32-cmake-test/build/CMakeFiles/stm32-cmake-test.bin.dir/cmake_clean.cmake
@@ -0,0 +1,8 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/stm32-cmake-test.bin"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang)
+  INCLUDE(CMakeFiles/stm32-cmake-test.bin.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
---
+++ trunk/stm32-cmake-test/build/CMakeFiles/stm32-cmake-test.hex.dir/cmake_clean.cmake
@@ -0,0 +1,8 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/stm32-cmake-test.hex"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang)
+  INCLUDE(CMakeFiles/stm32-cmake-test.hex.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
