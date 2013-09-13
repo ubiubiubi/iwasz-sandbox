39851f4724af86bc2360f1170435f28f52fdfbd2
lukasz.iwaszkiewicz@gmail.com <lukasz.iwaszkiewicz@gmail.com@e6fd12c7-fa05-65f5-2425-0ae74a4147e3>
2013-09-13 14:36:33 +0000

    trash

--- trunk/i2c-test/build/CMakeFiles/i2c-test.hex.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-FILE(REMOVE_RECURSE
-  "CMakeFiles/i2c-test.hex"
-)
-
-# Per-language clean rules from dependency scanning.
-FOREACH(lang)
-  INCLUDE(CMakeFiles/i2c-test.hex.dir/cmake_clean_${lang}.cmake OPTIONAL)
-ENDFOREACH(lang)
