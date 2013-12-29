8a94fb91746e37a5cbadce68068c86f80f913314
lukasz.iwaszkiewicz@gmail.com <lukasz.iwaszkiewicz@gmail.com@e6fd12c7-fa05-65f5-2425-0ae74a4147e3>
2013-12-29 22:46:59 +0000

    Project initial commit

---
+++ stm32-drama-button/build/CMakeFiles/i2c-test.bin.dir/cmake_clean.cmake
@@ -0,0 +1,8 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/i2c-test.bin"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang)
+  INCLUDE(CMakeFiles/i2c-test.bin.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
