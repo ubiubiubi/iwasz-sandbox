beeb2478e3b2b72a19b5da066b1133071b8700d5
lukasz.iwaszkiewicz@gmail.com <lukasz.iwaszkiewicz@gmail.com@e6fd12c7-fa05-65f5-2425-0ae74a4147e3>
2014-01-01 22:00:19 +0000

    Keypress A working

--- stm32-drama-button/src/usbd_conf.h
+++ stm32-drama-button/src/usbd_conf.h
@@ -6,7 +6,8 @@
 #define HID_IN_EP                    0x81
 #define HID_OUT_EP                   0x01
 
-#define HID_IN_PACKET                8
+#define HID_IN_PACKET                2
+//#define HID_IN_PACKET                8
 #define HID_OUT_PACKET               4
 
 #define USBD_CFG_MAX_NUM 1
--- stm32-drama-button/src/usbd_hid_core.h
+++ stm32-drama-button/src/usbd_hid_core.h
@@ -48,7 +48,8 @@
 #define USB_HID_CONFIG_DESC_SIZ       34
 #define USB_HID_DESC_SIZ              9
 //#define HID_KEYBOARD_REPORT_DESC_SIZE 63
-#define HID_KEYBOARD_REPORT_DESC_SIZE 45
+//#define HID_KEYBOARD_REPORT_DESC_SIZE 45
+#define HID_KEYBOARD_REPORT_DESC_SIZE 35
 //#define HID_KEYBOARD_REPORT_DESC_SIZE 74
 
 #define HID_DESCRIPTOR_TYPE           0x21
@@ -63,35 +64,20 @@
 
 #define HID_REQ_SET_REPORT            0x09
 #define HID_REQ_GET_REPORT            0x01
-/**
-  * @}
-  */ 
 
-
-/** @defgroup USBD_CORE_Exported_TypesDefinitions
-  * @{
-  */
-
-
-/**
-  * @}
-  */ 
-
-
-
-/** @defgroup USBD_CORE_Exported_Macros
-  * @{
-  */ 
-
-/**
-  * @}
-  */ 
-
-/** @defgroup USBD_CORE_Exported_Variables
-  * @{
-  */ 
+//#define MY_KEY_KEYCODE 0x7f
+#define MY_KEY_KEYCODE 0x04
+// #define MY_KEY_KEYCODE 0x45 // F12
 
 extern USBD_Class_cb_TypeDef  USBD_HID_cb;
+extern uint8_t keyboardReport[HID_IN_PACKET];
+extern uint8_t lastKey;
+
+
+extern uint32_t  USBD_HID_AltSet;
+extern uint32_t  USBD_HID_Protocol;
+extern uint32_t  USBD_HID_IdleState;
+
 /**
   * @}
   */ 
@@ -99,9 +85,9 @@ extern USBD_Class_cb_TypeDef  USBD_HID_cb;
 /** @defgroup USB_CORE_Exported_Functions
   * @{
   */ 
-uint8_t USBD_HID_SendReport (USB_OTG_CORE_HANDLE  *pdev, 
-                                 uint8_t *report,
-                                 uint16_t len);
+uint8_t USBD_HID_SendReport (USB_OTG_CORE_HANDLE  *pdev);
+extern void buildReport (uint8_t modifier, uint8_t key);
+
 /**
   * @}
   */ 
