dc44b5ab2f6bbe6ee9bff43c4018c9d85d795dfb
lukasz.iwaszkiewicz@gmail.com <lukasz.iwaszkiewicz@gmail.com@e6fd12c7-fa05-65f5-2425-0ae74a4147e3>
2014-01-02 12:32:40 +0000

    Drama button for stmf105

---
+++ stm32f105-drama-button/build/CMakeFiles/i2c-test.elf.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/i2c-test.elf.dir/home/iwasz/workspace/i2c-test/src/system_stm32f4xx.c.o"
+  "CMakeFiles/i2c-test.elf.dir/home/iwasz/workspace/i2c-test/src/main.c.o"
+  "CMakeFiles/i2c-test.elf.dir/home/iwasz/local/share/STM32F4-Discovery_FW_V1.1.0/Libraries/CMSIS/ST/STM32F4xx/Source/Templates/TrueSTUDIO/startup_stm32f4xx.s.o"
+  "CMakeFiles/i2c-test.elf.dir/home/iwasz/local/share/STM32F4-Discovery_FW_V1.1.0/Libraries/STM32F4xx_StdPeriph_Driver/src/stm32f4xx_rcc.c.o"
+  "CMakeFiles/i2c-test.elf.dir/home/iwasz/local/share/STM32F4-Discovery_FW_V1.1.0/Libraries/STM32F4xx_StdPeriph_Driver/src/stm32f4xx_gpio.c.o"
+  "CMakeFiles/i2c-test.elf.dir/home/iwasz/local/share/STM32F4-Discovery_FW_V1.1.0/Libraries/STM32F4xx_StdPeriph_Driver/src/stm32f4xx_i2c.c.o"
+  "i2c-test.elf.pdb"
+  "i2c-test.elf"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang ASM-ATT C)
+  INCLUDE(CMakeFiles/i2c-test.elf.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
---
+++ stm32f105-drama-button/build/CMakeFiles/i2c-test.hex.dir/cmake_clean.cmake
@@ -0,0 +1,8 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/i2c-test.hex"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang)
+  INCLUDE(CMakeFiles/i2c-test.hex.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
