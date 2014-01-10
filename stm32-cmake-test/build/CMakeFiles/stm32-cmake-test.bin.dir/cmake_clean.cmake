6bfb594c91f2b3f179182b23374afe2db742101b
lukasz.iwaszkiewicz@gmail.com <lukasz.iwaszkiewicz@gmail.com@e6fd12c7-fa05-65f5-2425-0ae74a4147e3>
2014-01-10 21:32:04 +0000

    After few bugs finally I got it working.

--- stm32-cmake-test/build/CMakeFiles/stm32-cmake-test.bin.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-FILE(REMOVE_RECURSE
-  "CMakeFiles/stm32-cmake-test.bin"
-)
-
-# Per-language clean rules from dependency scanning.
-FOREACH(lang)
-  INCLUDE(CMakeFiles/stm32-cmake-test.bin.dir/cmake_clean_${lang}.cmake OPTIONAL)
-ENDFOREACH(lang)
--- stm32-cmake-test/build/CMakeFiles/stm32-cmake-test.hex.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-FILE(REMOVE_RECURSE
-  "CMakeFiles/stm32-cmake-test.hex"
-)
-
-# Per-language clean rules from dependency scanning.
-FOREACH(lang)
-  INCLUDE(CMakeFiles/stm32-cmake-test.hex.dir/cmake_clean_${lang}.cmake OPTIONAL)
-ENDFOREACH(lang)
