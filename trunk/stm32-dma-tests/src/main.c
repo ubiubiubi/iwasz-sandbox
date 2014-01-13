859f7fa796cbb95b4f7407ded64caccccfe0c2a7
lukasz.iwaszkiewicz@gmail.com <lukasz.iwaszkiewicz@gmail.com@e6fd12c7-fa05-65f5-2425-0ae74a4147e3>
2014-01-13 23:44:11 +0000

    DMA OK, two transfers

--- trunk/stm32-dma-tests/src/main.c
+++ trunk/stm32-dma-tests/src/main.c
@@ -1,77 +1,23 @@
 #include <stm32f4xx.h>
-#include <stdio.h>
 #include "logf.h"
 
 /**
- * For printf.
+ * For printf, and USART1 in general.
  */
 void initUsart (void)
 {
-#define USE_USART 1
-#define USE_PIN_SET 1
-
-#if USE_USART == 1
-#define USART USART1
-#define RCC_USART RCC_APB2Periph_USART1
-#define RCC_APBxPeriphClockCmd RCC_APB2PeriphClockCmd
-#define GPIO_AF_USART GPIO_AF_USART1
-
-#if USE_PIN_SET == 1
-#define RCC_GPIO RCC_AHB1Periph_GPIOB
-#define PORT GPIOB
-#define TX_PIN_SOURCE GPIO_PinSource6
-#define RX_PIN_SOURCE GPIO_PinSource7
-#define TX_PIN GPIO_Pin_6
-#define RX_PIN GPIO_Pin_7
-#endif
-
-#if USE_PIN_SET == 2
-#define RCC_GPIO RCC_AHB1Periph_GPIOA
-#define PORT GPIOA
-#define TX_PIN_SOURCE GPIO_PinSource9
-#define RX_PIN_SOURCE GPIO_PinSource10
-#define TX_PIN GPIO_Pin_9
-#define RX_PIN GPIO_Pin_10
-#endif
-#endif
-
-#if USE_USART == 2
-#define USART USART2
-#define RCC_USART RCC_APB1Periph_USART2
-#define RCC_APBxPeriphClockCmd RCC_APB1PeriphClockCmd
-#define GPIO_AF_USART GPIO_AF_USART2
-
-#if USE_PIN_SET == 1
-#define RCC_GPIO RCC_AHB1Periph_GPIOA
-#define PORT GPIOA
-#define TX_PIN_SOURCE GPIO_PinSource2
-#define RX_PIN_SOURCE GPIO_PinSource3
-#define TX_PIN GPIO_Pin_2
-#define RX_PIN GPIO_Pin_3
-#endif
-
-#if USE_PIN_SET == 2
-#define RCC_GPIO RCC_AHB1Periph_GPIOA
-#define PORT GPIOA
-#define TX_PIN_SOURCE GPIO_PinSource9
-#define RX_PIN_SOURCE GPIO_PinSource10
-#define TX_PIN GPIO_Pin_9
-#define RX_PIN GPIO_Pin_10
-#endif
-#endif
-
-        RCC_APBxPeriphClockCmd (RCC_USART, ENABLE);
+        RCC_APB2PeriphClockCmd (RCC_APB2Periph_USART1, ENABLE);
         GPIO_InitTypeDef gpioInitStruct;
 
-        RCC_AHB1PeriphClockCmd (RCC_GPIO, ENABLE);
-        gpioInitStruct.GPIO_Pin = TX_PIN | RX_PIN;
+        RCC_AHB1PeriphClockCmd (RCC_AHB1Periph_GPIOB, ENABLE);
+        gpioInitStruct.GPIO_Pin = GPIO_Pin_6 | GPIO_Pin_7;
         gpioInitStruct.GPIO_Mode = GPIO_Mode_AF;
         gpioInitStruct.GPIO_Speed = GPIO_High_Speed;
         gpioInitStruct.GPIO_OType = GPIO_OType_PP;
         gpioInitStruct.GPIO_PuPd = GPIO_PuPd_UP;
-        GPIO_Init (PORT, &gpioInitStruct);
-        GPIO_PinAFConfig (PORT, TX_PIN_SOURCE, GPIO_AF_USART); // TX
-        GPIO_PinAFConfig (PORT, RX_PIN_SOURCE, GPIO_AF_USART); // RX
+        GPIO_Init (GPIOB, &gpioInitStruct);
+        GPIO_PinAFConfig (GPIOB, GPIO_PinSource6, GPIO_AF_USART1); // TX
+        GPIO_PinAFConfig (GPIOB, GPIO_PinSource7, GPIO_AF_USART1); // RX
 
         USART_InitTypeDef usartInitStruct;
         usartInitStruct.USART_BaudRate = 9600;
@@ -80,11 +26,11 @@ void initUsart (void)
         usartInitStruct.USART_Parity = USART_Parity_No;
         usartInitStruct.USART_Mode = USART_Mode_Rx | USART_Mode_Tx;
         usartInitStruct.USART_HardwareFlowControl = USART_HardwareFlowControl_None;
-        USART_Init (USART, &usartInitStruct);
-        USART_Cmd (USART, ENABLE);
+        USART_Init (USART1, &usartInitStruct);
+        USART_Cmd (USART1, ENABLE);
 }
 
-uint8_t dummyStrlen (char const *s)
+uint8_t myStrlen (char const *s)
 {
         uint8_t len = 0;
 
@@ -95,37 +41,15 @@ uint8_t dummyStrlen (char const *s)
         return len;
 }
 
-int main (void)
+/**
+ * Test1
+ */
+void initDma (char const *outputBuffer)
 {
-        /* This would be a function parameter or something like that. */
-        char *outputBuffer = "Ala ma kota, a kot ma ale, to jest taki wierszyk z czytanki dla dzieci, ktora jest tylko w Polsce.\r\n";
-
-        /*
-         * Enable the peripheral clock for DMA2. I want to use DMA with USART1, so according to
-         * table 44 in reference manual for STM32F407 (RM0090) this would be DMA2 peripheral.
-         * Description in stm32f4xx_dma.c advises to do this as the first operarion.
-         */
-
-        /*
-         * Spend two fu.king nights on this. Docs says to use RCC_AHB1PeriphResetCmd, but use
-         * RCC_AHB1PeriphClockCmd instead
-         */
-//        RCC_AHB1PeriphResetCmd (RCC_AHB1Periph_DMA2, ENABLE);
-        RCC_AHB1PeriphClockCmd (RCC_AHB1Periph_DMA2, ENABLE);
-
-/*--------------------------------------------------------------------------*/
-
-        /*
-         * Enable the USART1 device as usual.
-         */
-        initUsart ();
-        logf("Init\r\n");
-
-/*--------------------------------------------------------------------------*/
-
         /*
          * Reset DMA Stream registers (for debug purpose). For DMA2_Stream7 exmplanation read on.
-         * It also disables the stream.
+         * It also disables the stream. Stream must be disabled prior configure it. Otherwise it can
+         * misbehave.
          */
         DMA_DeInit (DMA2_Stream7);
 
@@ -151,14 +75,20 @@ int main (void)
          */
         dmaInitStructure.DMA_Channel = DMA_Channel_4;
 
-        /* Possible values : DMA_DIR_PeripheralToMemory, DMA_DIR_MemoryToPeripheral, DMA_DIR_MemoryToMemory. */
+        /*
+         * Possible values : DMA_DIR_PeripheralToMemory, DMA_DIR_MemoryToPeripheral,
+         * DMA_DIR_MemoryToMemory.
+         */
         dmaInitStructure.DMA_DIR = DMA_DIR_MemoryToPeripheral;
 
         /* Why DMA_PeripheralBaseAddr is of type uint32_t? Shouldn't it be void *? */
         dmaInitStructure.DMA_PeripheralBaseAddr = (uint32_t)&(USART1->DR);
         dmaInitStructure.DMA_Memory0BaseAddr = (uint32_t)outputBuffer;
 
-        /* Only valid values here are : DMA_PeripheralDataSize_Byte, DMA_PeripheralDataSize_HalfWord, DMA_PeripheralDataSize_Word */
+        /*
+         * Only valid values here are : DMA_PeripheralDataSize_Byte, DMA_PeripheralDataSize_HalfWord,
+         * DMA_PeripheralDataSize_Word
+         */
         dmaInitStructure.DMA_PeripheralDataSize = DMA_PeripheralDataSize_Byte;
 
         /*
@@ -170,14 +100,14 @@ int main (void)
 
         /*
          * Length of the data to be transferred by the DMA. Unit of this length is DMA_MemoryDataSize when
-         * direction is from memory to peripheral or DMA_PeripheralDataSize otherwise. Since I set both sizes
-         * to one byte, I simply put strlen here.
+         * direction is from memory to peripheral or DMA_PeripheralDataSize otherwise. Since I set both
+         * sizes to one byte, I simply put strlen here.
          */
-        dmaInitStructure.DMA_BufferSize = dummyStrlen (outputBuffer);
+        dmaInitStructure.DMA_BufferSize = myStrlen (outputBuffer);
 
         /*
-         * DMA_PeripheralInc_Disable means to read or to write to the same location everytime. DMA_MemoryInc_Enable would
-         * increase memory or peripheral location after each read/write.
+         * DMA_PeripheralInc_Disable means to read or to write to the same location everytime.
+         * DMA_MemoryInc_Enable would increase memory or peripheral location after each read/write.
          */
         dmaInitStructure.DMA_PeripheralInc = DMA_PeripheralInc_Disable;
         dmaInitStructure.DMA_MemoryInc = DMA_MemoryInc_Enable;
@@ -198,8 +128,9 @@ int main (void)
         dmaInitStructure.DMA_FIFOThreshold = DMA_FIFOThreshold_Full;
 
         /*
-         * Specifies whether to use single or busrt mode. If burst, then it specifies how much "beats" to use.
-         * DMA_MemoryBurst_Single, DMA_MemoryBurst_INC4, DMA_MemoryBurst_INC8 or DMA_MemoryBurst_INC16.
+         * Specifies whether to use single or busrt mode. If burst, then it specifies how much "beats"
+         * to use. DMA_MemoryBurst_Single, DMA_MemoryBurst_INC4, DMA_MemoryBurst_INC8 or
+         * DMA_MemoryBurst_INC16.
          */
         dmaInitStructure.DMA_MemoryBurst = DMA_MemoryBurst_Single;
         dmaInitStructure.DMA_PeripheralBurst = DMA_PeripheralBurst_Single;
@@ -234,12 +165,47 @@ int main (void)
                 while (1) {
                 }
         }
+}
+
+int main (void)
+{
+        /* This would be a function parameter or something like that. */
+        char *outputBufferA = "Ala ma kota, a kot ma ale, to jest taki wierszyk z czytanki dla dzieci, ktora jest tylko w Polsce.\r\n";
+        char *outputBufferB = "Wlazl kotek na plotek i mruga. Ladna to piosenka nie dluga. Nie dluga, nie krotka lecz w sam raz.\r\n";
+
+        /*
+         * Enable the peripheral clock for DMA2. I want to use DMA with USART1, so according to
+         * table 44 in reference manual for STM32F407 (RM0090) this would be DMA2 peripheral.
+         * Description in stm32f4xx_dma.c advises to do this as the first operarion.
+         */
+
+        /*
+         * Spend two fu.king nights on this. Docs says to use RCC_AHB1PeriphResetCmd, but use
+         * RCC_AHB1PeriphClockCmd instead
+         */
+//        RCC_AHB1PeriphResetCmd (RCC_AHB1Periph_DMA2, ENABLE);
+        RCC_AHB1PeriphClockCmd (RCC_AHB1Periph_DMA2, ENABLE);
+
+/*--------------------------------------------------------------------------*/
+
+        /*
+         * Enable the USART1 device as usual.
+         */
+        initUsart ();
+        logf("Init\r\n");
+
+/*--------------------------------------------------------------------------*/
+
+        initDma (outputBufferA);
 
         /*
          * The DMA stream is turned on now and waits for DMA requests. As far as I know, if this
          * were to be memory-to-memory transfer, it would start immedialtely without enabling any
          * channels. But for peripherals one has to enable the channel for requests. After following
          * statement, you should see data on serial console.
+         *
+         * This statement enables the DMA internals in USART (this stuff which communicates with the DMA
+         * controller).
          */
         USART_DMACmd (USART1, USART_DMAReq_Tx, ENABLE);
 
@@ -250,10 +216,22 @@ int main (void)
         while (DMA_GetFlagStatus (DMA2_Stream7, DMA_FLAG_TCIF7) == RESET)
                 ;
 
+        logf("It worked, and didn't hanged\r\n");
+
         /* Clear DMA Transfer Complete Flags */
         DMA_ClearFlag (DMA2_Stream7, DMA_FLAG_TCIF7);
 
-        logf("It worked, and didn't hanged\r\n");
+        /* Th has to be initialized once again AFAIK to send another portion of data. */
+        initDma (outputBufferB);
+
+        /* Try to start it again */
+        USART_DMACmd (USART1, USART_DMAReq_Tx, ENABLE);
+
+        /* Waiting the end of Data transfer */
+        while (USART_GetFlagStatus (USART1, USART_FLAG_TC) == RESET)
+                ;
+
+        logf("It workedagain\r\n");
 
         /* Infinite loop */
         while (1) {
