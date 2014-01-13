48013c5bb96938fb25e01a435c021bd87401ac7e
lukasz.iwaszkiewicz@gmail.com <lukasz.iwaszkiewicz@gmail.com@e6fd12c7-fa05-65f5-2425-0ae74a4147e3>
2014-01-13 23:44:46 +0000

    tag

--- stm32-test2/build/CMakeFiles/stm32-test2.bin.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-FILE(REMOVE_RECURSE
-  "CMakeFiles/stm32-test2.bin"
-)
-
-# Per-language clean rules from dependency scanning.
-FOREACH(lang)
-  INCLUDE(CMakeFiles/stm32-test2.bin.dir/cmake_clean_${lang}.cmake OPTIONAL)
-ENDFOREACH(lang)
--- stm32-test2/build/CMakeFiles/stm32-test2.hex.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-FILE(REMOVE_RECURSE
-  "CMakeFiles/stm32-test2.hex"
-)
-
-# Per-language clean rules from dependency scanning.
-FOREACH(lang)
-  INCLUDE(CMakeFiles/stm32-test2.hex.dir/cmake_clean_${lang}.cmake OPTIONAL)
-ENDFOREACH(lang)
